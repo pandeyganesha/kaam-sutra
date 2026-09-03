9adec184d126d6409f8f96553baea5fccb9d09d1
pandeyganesha <pandeyganesha@gmail.com>
2026-09-03 16:50:31 +0530

    feat: simplify GoalScreen and AddGoalDialog by removing unnecessary parameters and enhancing state management

--- app/src/main/java/com/pandeyganesha/kaamsutra/MainActivity.kt
+++ app/src/main/java/com/pandeyganesha/kaamsutra/MainActivity.kt
@@ -8,21 +8,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.pandeyganesha.kaamsutra.ui.theme.KaamSutraTheme
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.rememberCoroutineScope
-import kotlin.collections.emptyList
-import androidx.compose.runtime.collectAsState
-import com.pandeyganesha.kaamsutra.data.DatabaseProvider
 import com.pandeyganesha.kaamsutra.data.Habit
-import kotlinx.coroutines.launch
-import com.pandeyganesha.kaamsutra.ui.components.DeleteTaskDialog
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import java.time.LocalDate
-import com.pandeyganesha.kaamsutra.data.HabitLog
 import android.Manifest
 import android.os.Build
 import androidx.activity.result.contract.ActivityResultContracts
@@ -33,26 +21,13 @@ import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.FloatingActionButton
 import com.pandeyganesha.kaamsutra.data.scheduleTestNotification
 import com.pandeyganesha.kaamsutra.data.scheduleMissedHabitSettlement
 import com.pandeyganesha.kaamsutra.ui.components.AppTopBar
 import com.pandeyganesha.kaamsutra.ui.components.habits.HabitScreen
-import androidx.compose.material.icons.Icons
-import androidx.compose.material3.Icon
-import androidx.compose.material.icons.filled.Add
-import com.pandeyganesha.kaamsutra.data.Goal
-import com.pandeyganesha.kaamsutra.data.Status
-import com.pandeyganesha.kaamsutra.data.Tag
-import com.pandeyganesha.kaamsutra.data.Todo
-import com.pandeyganesha.kaamsutra.data.TodoTag
 import com.pandeyganesha.kaamsutra.ui.components.goals.GoalScreen
 import com.pandeyganesha.kaamsutra.ui.components.todos.TodoScreen
-import com.pandeyganesha.kaamsutra.ui.components.EmptyState
-import com.pandeyganesha.kaamsutra.ui.components.goals.AddGoalDialog
-import com.pandeyganesha.kaamsutra.ui.components.habits.AddHabitDialog
 import com.pandeyganesha.kaamsutra.ui.components.habits.RepeatType
-import com.pandeyganesha.kaamsutra.ui.components.todos.AddTodoDialog
 import java.time.DayOfWeek
 
 
@@ -107,40 +82,9 @@ fun KaamSutraApp(screenToOpen: Screen) {
         initialPage = Screen.GOALS.ordinal,
         pageCount = { Screen.entries.size}
     )
-    val db = (LocalContext.current.applicationContext as MyApp).db
-    val scope = rememberCoroutineScope()
-
-
-    val goalDao = db.goalDao()
-    val coroutineScope = rememberCoroutineScope()
     val currentScreen = Screen.entries[pagerState.currentPage]
-    val activeGoals by goalDao.getGoals(Status.ACTIVE).collectAsState(initial = emptyList())
-
-    val isEmptyMap = mapOf(
-        Screen.GOALS to activeGoals.isEmpty()
-    )
-    val existingNames = remember(activeGoals) {
-        mapOf(
-            Screen.GOALS to activeGoals.map { it.name }.toSet()
-        )
-    }
-    var showDialog by remember { mutableStateOf(false) }
-    var goalBeingEdited by remember { mutableStateOf<Goal?>(null) }
-    var goalBeingDeleted by remember { mutableStateOf<Goal?>(null) }
-
-
-
 
     Scaffold(
-        floatingActionButton = {
-            if (currentScreen == Screen.GOALS) {
-                FloatingActionButton(
-                    onClick = { showDialog = true }) {
-                    Icon(Icons.Default.Add, contentDescription = "Add")
-                }
-            }
-
-        },
         topBar = {
             AppTopBar(
                 title = currentScreen.plural,
@@ -153,82 +97,13 @@ fun KaamSutraApp(screenToOpen: Screen) {
             modifier = Modifier
                 .fillMaxSize()
         ) { page ->
-            if (isEmptyMap[currentScreen] ?: false) {
-                EmptyState(pageName = currentScreen, onClick = { showDialog = true })
-            }
-            else {
-                when (Screen.entries[page]) {
-                    Screen.HABITS -> {
-                        HabitScreen(modifier = Modifier.padding(innerPadding))
-                    }
-                    Screen.GOALS -> GoalScreen(
-                        activeGoals = activeGoals,
-                        onCheckedChange = { checked, goal ->
-                            coroutineScope.launch {
-                                goalDao.updateGoal(goal.copy(completed = checked))
-                            }
-                        },
-                        onEditClicked = { goal -> goalBeingEdited = goal },
-                        onDeleteClicked = { goal -> goalBeingDeleted = goal },
-                        onSortOrderUpdate = { reorderedList ->
-                            scope.launch {
-                                db.goalDao().updateGoals(reorderedList)
-                            }
-                        },
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            when (Screen.entries[page]) {
+                Screen.HABITS ->
+                    HabitScreen(modifier = Modifier.padding(innerPadding))
 
-                    Screen.TODO -> TodoScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Screen.GOALS -> GoalScreen(modifier = Modifier.padding(innerPadding))
+                Screen.TODO -> TodoScreen(modifier = Modifier.padding(innerPadding))
             }
-    }
-    }
-
-
-    goalBeingDeleted?.let { goal ->
-        DeleteTaskDialog(
-            taskName = goal.name,
-            onDismiss = { goalBeingDeleted = null },
-            onConfirm = {
-                coroutineScope.launch {
-                    goalDao.softDeleteGoal(goal.id)
-                    goalBeingDeleted = null
-                }
-            }
-        )
-    }
-    goalBeingEdited?.let { goal ->
-        AddGoalDialog(
-            goal = goal,
-            existingGoalNames = existingNames[currentScreen].orEmpty(),
-            currentScreen = currentScreen,
-            onDismiss = {
-                goalBeingEdited = null
-            },
-            onConfirm = { goalName ->
-                coroutineScope.launch {
-                    goalDao.updateGoal(goal.copy(name = goal.name.replaceFirstChar { it.uppercase() }))
-                    goalBeingEdited = null
-                }
-            }
-        )
-    }
-    if (showDialog) {
-        when (currentScreen) {
-            Screen.GOALS ->
-                AddGoalDialog(
-                    existingGoalNames = existingNames[currentScreen].orEmpty(),
-                    currentScreen = currentScreen,
-                    onDismiss = { showDialog = false },
-                    onConfirm = { goal ->
-                        coroutineScope.launch {
-                            goalDao.createGoal(goal.copy(name = goal.name.replaceFirstChar { it.uppercase() }))
-                        }
-                        showDialog = false
-                    })
-            else -> {}
         }
     }
 }
--- app/src/main/java/com/pandeyganesha/kaamsutra/ui/components/goals/GoalScreen.kt
+++ app/src/main/java/com/pandeyganesha/kaamsutra/ui/components/goals/GoalScreen.kt
@@ -16,18 +16,31 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import sh.calvin.reorderable.ReorderableItem
-import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import com.pandeyganesha.kaamsutra.MyApp
+import com.pandeyganesha.kaamsutra.Screen
+import com.pandeyganesha.kaamsutra.data.Status
 import com.pandeyganesha.kaamsutra.ui.components.CollapsibleSectionHeader
+import com.pandeyganesha.kaamsutra.ui.components.DeleteTaskDialog
+import com.pandeyganesha.kaamsutra.ui.components.EmptyState
+import kotlinx.coroutines.launch
 
 
 @Composable
-fun GoalScreen(activeGoals: List<Goal>,
-               onCheckedChange: (Boolean, Goal) -> Unit,
-               onEditClicked: (Goal) -> Unit,
-               onDeleteClicked: (Goal) -> Unit,
-               onSortOrderUpdate: (List<Goal>) -> Unit,
-               modifier: Modifier
+fun GoalScreen(
+    modifier: Modifier
 ) {
+    val db = (LocalContext.current.applicationContext as MyApp).db
+    val coroutineScope = rememberCoroutineScope()
+    val scope = rememberCoroutineScope()
+    val goalDao = db.goalDao()
+    val activeGoals by goalDao.getGoals(Status.ACTIVE).collectAsState(initial = emptyList())
+    var goalBeingEdited by remember { mutableStateOf<Goal?>(null) }
+    var goalBeingDeleted by remember { mutableStateOf<Goal?>(null) }
+    val existingGoals = activeGoals.map { it.name }.toSet()
+    var showDialog by remember { mutableStateOf(false) }
     val lazyListState = rememberLazyListState()
     var goalsNotDone by remember(activeGoals) {
         mutableStateOf(activeGoals.filter { !it.completed })
@@ -39,52 +52,111 @@ fun GoalScreen(activeGoals: List<Goal>,
             add(to.index, removeAt(from.index))
         }
     }
-
-    LazyColumn(state = lazyListState, modifier = modifier.fillMaxSize()) {
-        items(goalsNotDone, key = { it.id }) { goal ->
-            ReorderableItem(reorderableState, key = goal.id) { isDragging ->
-
-                GoalRow(
-                    goal = goal,
-                    isChecked = goal.completed,
-                    onCheckedChange = { checked -> onCheckedChange(checked, goal) },
-                    onEditClick = { onEditClicked(goal) },
-                    onDeleteClick = { onDeleteClicked(goal) },
-                    modifier = Modifier.longPressDraggableHandle(
-                        onDragStopped = {
-                            val size = goalsNotDone.size
-                            val reordered = goalsNotDone.mapIndexed { index, t ->
-                                t.copy(sortOrder = size - 1 - index)
-                            }
-                            goalsNotDone = reordered
-                            onSortOrderUpdate(reordered)
-                        }
-                    )
-                )
-            }
+    val onCheckedChange: (Boolean, Goal) -> Unit = { checked, goal ->
+        coroutineScope.launch {
+            goalDao.updateGoal(goal.copy(completed = checked))
         }
-        if (goalsDone.isNotEmpty()) {
-            item {
-                CollapsibleSectionHeader(
-                    title = "Done",
-                    expanded = doneExpanded,
-                    onToggle = { doneExpanded = !doneExpanded }
-                )
-            }
-            if (doneExpanded) {
-                items(goalsDone, key = { it.id }) { goal ->
+    }
+    val onEditClicked: (Goal) -> Unit = { goal -> goalBeingEdited = goal }
+    val onDeleteClicked: (Goal) -> Unit = { goal -> goalBeingDeleted = goal }
+    val onSortOrderUpdate: (List<Goal>) -> Unit = { reorderedList ->
+        scope.launch {
+            db.goalDao().updateGoals(reorderedList)
+        }
+    }
+
+
+
+    if (activeGoals.isEmpty()){
+        EmptyState(pageName = Screen.TODO, onClick = { showDialog = true })
+
+    }
+    else {
+        LazyColumn(state = lazyListState, modifier = modifier.fillMaxSize()) {
+            items(goalsNotDone, key = { it.id }) { goal ->
+                ReorderableItem(reorderableState, key = goal.id) { isDragging ->
+
                     GoalRow(
                         goal = goal,
                         isChecked = goal.completed,
                         onCheckedChange = { checked -> onCheckedChange(checked, goal) },
                         onEditClick = { onEditClicked(goal) },
-                        onDeleteClick = { onDeleteClicked(goal) }
+                        onDeleteClick = { onDeleteClicked(goal) },
+                        modifier = Modifier.longPressDraggableHandle(
+                            onDragStopped = {
+                                val size = goalsNotDone.size
+                                val reordered = goalsNotDone.mapIndexed { index, t ->
+                                    t.copy(sortOrder = size - 1 - index)
+                                }
+                                goalsNotDone = reordered
+                                onSortOrderUpdate(reordered)
+                            }
+                        )
                     )
                 }
             }
-            item {
-                Spacer(modifier = Modifier.height(75.dp))
+            if (goalsDone.isNotEmpty()) {
+                item {
+                    CollapsibleSectionHeader(
+                        title = "Done",
+                        expanded = doneExpanded,
+                        onToggle = { doneExpanded = !doneExpanded }
+                    )
+                }
+                if (doneExpanded) {
+                    items(goalsDone, key = { it.id }) { goal ->
+                        GoalRow(
+                            goal = goal,
+                            isChecked = goal.completed,
+                            onCheckedChange = { checked -> onCheckedChange(checked, goal) },
+                            onEditClick = { onEditClicked(goal) },
+                            onDeleteClick = { onDeleteClicked(goal) }
+                        )
+                    }
+                }
+                item {
+                    Spacer(modifier = Modifier.height(75.dp))
+                }
             }
         }
+    }
+    goalBeingDeleted?.let { goal ->
+        DeleteTaskDialog(
+            taskName = goal.name,
+            onDismiss = { goalBeingDeleted = null },
+            onConfirm = {
+                coroutineScope.launch {
+                    goalDao.softDeleteGoal(goal.id)
+                    goalBeingDeleted = null
+                }
+            }
+        )
+    }
+    goalBeingEdited?.let { goal ->
+        AddGoalDialog(
+            goal = goal,
+            existingGoalNames = existingGoals,
+            onDismiss = {
+                goalBeingEdited = null
+            },
+            onConfirm = { goalName ->
+                coroutineScope.launch {
+                    goalDao.updateGoal(goal.copy(name = goal.name.replaceFirstChar { it.uppercase() }))
+                    goalBeingEdited = null
+                }
+            }
+        )
+    }
+
+    if (showDialog){
+        AddGoalDialog(
+            existingGoalNames = existingGoals,
+            onDismiss = { showDialog = false },
+            onConfirm = { goal ->
+                coroutineScope.launch {
+                    goalDao.createGoal(goal.copy(name = goal.name.replaceFirstChar { it.uppercase() }))
+                }
+                showDialog = false
+            })
     }
 }
